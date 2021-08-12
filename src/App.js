b1b5347ebb347baabaa40229f6f1a0dfb5c9133f
seohee <jsh001505@gmail.com>
2021-08-12 18:19:04 +0900

    흐르는 시간에 따른 제어문 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState, useContext } from "react";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
+import useSound from "use-sound";
 
 const useStyles = makeStyles({
 	root: {
@@ -14,25 +15,63 @@ const useStyles = makeStyles({
 
 const App = () => {
 	const classes = useStyles();
-	const [second, setSecond] = useState(55);
+	const [second, setSecond] = useState(0);
 	const [minute, setMinute] = useState(0);
 	const [hour, setHour] = useState(0);
+	const [allSecond, setAllSecond] = useState(second);
+	const [allMinute, setAllMinute] = useState(minute);
+	const [allHour, setAllHour] = useState(hour);
 	const sleep = (milliseconds) => {
 		return new Promise((resolve) => setTimeout(resolve, milliseconds));
 	};
 
-	const upMinute = () => {
-		if (minute === 59) {
-			setMinute(0);
-			setHour(hour + 1);
-		} else setMinute(minute + 1);
-	};
-
+	// 초 증가
 	const upSecond = () => {
 		if (second === 59) {
 			setSecond(0);
 			upMinute();
 		} else setSecond(second + 1);
+		upAllSecond();
+	};
+
+	// 분 증가
+	const upMinute = () => {
+		if (minute === 59) {
+			setMinute(0);
+			setHour(hour + 1);
+			upAllHour();
+		} else setMinute(minute + 1);
+		upAllMinute();
+	};
+
+	// 초 합계 증가 (메소회수 계산용)
+	const upAllSecond = () => {
+		setAllSecond(allSecond + 1);
+		if (allSecond > 0 && allSecond % 104 === 0) {
+			console.log("all Second : ", allSecond);
+			console.log("메소회수타임");
+		}
+	};
+
+	// 분 합계 증가 (15분 , 30분 계산용)
+	const upAllMinute = () => {
+		setAllMinute(allMinute + 1);
+		if (allMinute > 0 && allMinute % 14 === 0) {
+			console.log("all Minute : ", allMinute, minute);
+			console.log("15분 경쿠 사용");
+		} else if (allMinute > 0 && allMinute % 29 === 0) {
+			console.log("all Minute : ", allMinute, minute);
+			console.log("30분 버프 사용");
+		}
+	};
+
+	// 시 합계 증가 ( 2시간 재획비, 경축비 알림)
+	const upAllHour = () => {
+		setAllHour(allHour + 1);
+		if (allHour % 2 === 0) {
+			console.log("all Hour : ", allHour);
+			console.log("2시간 재획비, 경축비");
+		}
 	};
 
 	useEffect(() => {
@@ -48,6 +87,9 @@ const App = () => {
 			</Typography>
 			<Typography className={classes.title} variant="h3">
 				{hour} : {minute} : {second}
+			</Typography>
+			<Typography className={classes.title} variant="h3">
+				{allSecond}
 			</Typography>
 		</div>
 	);
